999f0addff2bbf4f657a0feda934e1b54160223f
slavik <slavikshen@gmail.com>
2014-04-22 15:16:55 -0700

    update test script

--- test.sh
+++ test.sh
@@ -1,17 +1,20 @@
-rostopic pub -1 /s1/command std_msgs/Float64 -- 1
-rostopic pub -1 /s3/command std_msgs/Float64 -- -1
+rostopic pub -1 /s1/command std_msgs/Float64 --  1
 rostopic pub -1 /s2/command std_msgs/Float64 -- -1
-rostopic pub -1 /s4/command std_msgs/Float64 -- 1
+
+rostopic pub -1 /s3/command std_msgs/Float64 --  0.5
+rostopic pub -1 /s4/command std_msgs/Float64 -- -0.5
 
 rostopic pub -1 /s5/command std_msgs/Float64 --  0.6
 rostopic pub -1 /s6/command std_msgs/Float64 -- -0.6
-rostopic pub -1 /s5/command std_msgs/Float64 --  0
-rostopic pub -1 /s6/command std_msgs/Float64 -- -0
 
-rostopic pub -1 /s1/command std_msgs/Float64 -- -1.18
-rostopic pub -1 /s3/command std_msgs/Float64 -- -1.11
-rostopic pub -1 /s2/command std_msgs/Float64 --  1.18
+rostopic pub -1 /s6/command std_msgs/Float64 --  1.53 
+rostopic pub -1 /s5/command std_msgs/Float64 -- -1.53
+
 rostopic pub -1 /s4/command std_msgs/Float64 --  1.11
+rostopic pub -1 /s3/command std_msgs/Float64 -- -1.11
+
+rostopic pub -1 /s2/command std_msgs/Float64 --  1.18
+rostopic pub -1 /s1/command std_msgs/Float64 -- -1.18
 
 
 
